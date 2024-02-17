1bfe2e3c9e72168618228b25166579968d77620c
AlexJNeville <157894487+AlexJNeville@users.noreply.github.com>
2024-02-17 20:31:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
 
-const element = document.querySelector('.my-element');
-
-// Define the options for the Intersection Observer
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.5
-};
-
-// Create a new Intersection Observer
-const observer = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    // If element is in viewport, add the 'show' class to trigger the animation
-    if (entry.isIntersecting) {
-      element.classList.add('show');
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)){
+        if(options.cb){
+            options.cb(el)
+        }else{
+            entry.target.classList.add('active')
+        }
+        return
     }
-    else {
-      element.classList.remove('show');
-    }
-  });
-}, options);
+    let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                if(options.cb){
+                    options.cb(el)
+                }else{
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+// Example usages:
+scrollTrigger('.intro-text')
 
-// Start observing the element
-observer.observe(element);
+scrollTrigger('.scroll-reveal', {
+    rootMargin: '-200px',
+})
+
+scrollTrigger('.loader', {
+    rootMargin: '-200px',
+    cb: function(el){
+        el.innerText = 'Loading...'
+        setTimeout(() => {
+            el.innerText = 'Task Complete!'
+        }, 1000)
+    }
+})
