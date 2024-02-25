092913129215899d2f074932533dbac92a9e64dd
AlexJNeville <157894487+AlexJNeville@users.noreply.github.com>
2024-02-25 09:24:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-window.addEventListener("scroll", setScrollVar)
-window.addEventListener("resize", setScrollVar)
+const boxes = document.
+querySelectorAll('.box');
+window.addEventListener('scroll', checkBoxes);
 
-function setScrollVar() {
-  const htmlElement = document.documentElement
-  const percentOfScreenHeightScrolled =
-    htmlElement.scrollTop / htmlElement.clientHeight
-  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
-  htmlElement.style.setProperty(
-    "--scroll",
-    Math.min(percentOfScreenHeightScrolled * 100, 100)
-  )
-}
+checkBoxes();
 
-setScrollVar()
+function checkBoxes() {
+  const triggerBottom = window.innerHeight/ 5 * 4;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
 
-const observer = new IntersectionObserver(entries => {
-  for (let i = entries.length - 1; i >= 0; i--) {
-    const entry = entries[i]
-    if (entry.isIntersecting) {
-      document.querySelectorAll("[data-img]").forEach(img => {
-        img.classList.remove("show")
-      })
-      const img = document.querySelector(entry.target.dataset.imgToShow)
-      img?.classList.add("show")
-      break
+    if (boxTop < triggerBottom) {
+    box.classList.add('show');
+    } else {
+      box.classList.remove('show');
     }
-  }
-})
-
-document.querySelectorAll("[data-img-to-show]").forEach(section => {
-  observer.observe(section)
-})
+  });
+}
